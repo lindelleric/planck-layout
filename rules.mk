1a572db6f3f05aa9523176a679d70b76a9d5eae5
Eric Nilsson <eric.nilsson@hotmail.com>
2021-05-28 10:07:32 +0200

    Init

---
+++ rules.mk
@@ -0,0 +1,8 @@
+ifndef QUANTUM_DIR
+	include ../../../../Makefile
+endif
+
+# EXTRAKEY_ENABLE = yes          # Audio control and System control
+AUDIO_ENABLE = no
+BACKLIGHT_ENABLE = yes
+UNICODE_ENABLE = no
